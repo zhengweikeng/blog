6419d95c0262fde53ed8cb1639a2ce36b21bab28
zhengweikeng <seed_1029@outlook.com>
2018-03-06 11:33:52 +0800

    update posts

--- posts/2018/javascript/n.js
+++ posts/2018/javascript/n.js
@@ -1,15 +1,21 @@
-var fs = require('fs')
-
-fs.readFile('./n.js', () => {
-  setTimeout(function () {
-   console.log(1)
-  }, 0)
-
-  setImmediate(function () {
-    console.log(2)
-  })
-
-  process.nextTick(function () {
-    console.log(3)
-  })
+setTimeout(function () {
+  console.log(1)
 })
+
+setImmediate(function () {
+  console.log(2)
+}) 
+
+process.nextTick(function () {
+  console.log(3)
+}) 
+
+const p = new Promise(function (resolve, reject) {
+  console.log(4)
+  resolve()
+})
+p.then(function () {
+  console.log(5)
+})
+
+console.log(6) 
