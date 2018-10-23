58d188f6a7b9f413a40f38bad4fffabb7c6940b3
zhengweikeng <seed_1029@outlook.com>
2018-10-23 19:07:04 +0800

    add posts

---
+++ posts/2018/算法/src/bst-recursive.js
@@ -0,0 +1,98 @@
+// 递归方式实习二叉查找树
+function createTree(key, value, left, right) {
+  return {key, value, left, right, size: 1}
+}
+
+function search(tree, key) {
+  if (tree == null) {
+    return null
+  }
+
+  if (tree.key < key) {
+    return search(tree.right, key) 
+  } else if(tree.key > key) {
+    return search(tree.left, key) 
+  } else return tree.value
+}
+
+function put(tree, key, value) {
+  if (tree == null) {
+    return createTree(key, value)
+  }
+
+  if (tree.key > key) {
+    tree.left = put(tree.left, key, value)
+  } else if (tree.key < key) {
+    tree.right = put(tree.right, key, value)
+  } else tree.value = value
+
+  tree.size = size(tree.left) + size(tree.right) + 1
+
+  return tree
+}
+
+function size(tree) {
+  if (tree == null) {
+    return 0
+  }
+  return tree.size
+}
+
+function min(node) {
+  if (node.left == null) return node
+
+  return min(node.left)
+}
+
+function max(node) {
+  if (node.right == null) return node
+
+  return max(node.right)
+}
+
+function floor(node, key) {
+  if (node == null) {
+    return null
+  }
+
+  if (key === node.key) {
+    return node
+  } else if (key < node.key) {
+    return floor(node.left, key)
+  } else {
+    const n = floor(node.right, key)
+    if (n != null) {
+      return n
+    } else return node
+  }
+}
+
+function ceiling(node, key) {
+  if (node == null) {
+    return null
+  }
+console.log(node.key);
+  if (key === node.key) {
+    return node
+  } else if (key < node.key) {
+    const n = ceiling(node.left, key)
+    if (n != null) {
+      return n
+    } else return node
+  } else {
+    return ceiling(node.right, key)
+  }
+}
+
+const tree = createTree(10, 'a')
+console.log(search(tree, 10));
+put(tree, 5, 'b')
+put(tree, 7, 'c')
+put(tree, 11, 'd')
+put(tree, 1, 'e')
+put(tree, 2, 'f')
+put(tree, 19, 'g')
+// console.log(tree, size(tree));
+// console.log(min(tree));
+// console.log(floor(tree, 6));
+console.log(ceiling(tree, 6));
---
+++ posts/2018/算法/src/bst.js
@@ -0,0 +1,55 @@
+// 非递归方式实现二叉查找树
+
+function createTree(key, value, left, right) {
+  return {key, value, left, right, size: 1}
+}
+
+function search(tree, key) {
+  let node = tree
+  while(node != null) {
+    if (node.key > key) {
+      node = node.left
+    } else if (node.key < key) {
+      node = node.right
+    } else return node.value
+  }
+
+  return null
+}
+
+function put(tree, key, value) {
+  let currNode = tree
+  let parentNode = null
+  const node = createTree(key, value)
+
+  while(true) {
+    parentNode = currNode
+    if (parentNode.key > key) {
+      currNode = currNode.left
+      if (currNode == null) {
+        parentNode.left = node
+        break
+      }
+    } else if (parentNode.key < key) {
+      currNode = currNode.right
+      if (currNode == null) {
+        parentNode.right = node
+        break
+      }
+    } else {
+      parentNode.value = value
+      break
+    }
+  }
+  
+  return tree
+}
+
+const tree = createTree(10, 'a')
+put(tree, 5, 'b')
+put(tree, 6, 'c')
+put(tree, 11, 'd')
+put(tree, 1, 'e')
+put(tree, 2, 'f')
+put(tree, 19, 'g')
+console.log(tree);
